67d4fb351c1861317664b317ebdc1ae662ea6ca1
Fierce <88373778+Prexry@users.noreply.github.com>
2023-10-24 18:21:31 -0500

    about:blank

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); // Add this line to import the fs module
+const fs = require('fs');
 
 const app = express();
 const port = 9001;
@@ -8,16 +8,23 @@ const port = 9001;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Custom middleware to handle URL redirection
+
+// Custom middleware to handle URL redirection and alert
 app.use((req, res, next) => {
   const filePath = path.join(__dirname, 'public', req.path);
-  
+
   // Check if the request has no file extension and the file doesn't exist
   if (!path.extname(req.path) && !fs.existsSync(filePath)) {
     // Redirect to the same URL with .html appended
     return res.redirect(req.path + '.html');
   }
-  
+
+  // Check if the URL doesn't start with "about:blank" or "file:"
+/*  if (!req.url.startsWith('about:blank') && !req.url.startsWith('file:')) {
+    // Display an alert if the URL doesn't match
+    return res.send('<script>alert("Get out of here, idiot.")</script>');
+  }*/
+
   next();
 });
 
